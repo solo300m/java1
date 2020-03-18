c89d58afe02c3b7f25785392eb5edb82f64767bc
Solo300m <solo300m@yandex.ru>
2020-03-18 21:09:16 +0400

    H6 Домашнее задание

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -21,25 +21,56 @@ class Pro{
         for(int i = 0; i<arrNew.length(); i++)
             System.out.print(arrNew.at(i)+" ");
         System.out.println();
+        DIntArray new1 = new DIntArray();
+        new1.add(3);
+        for(int i = 0; i<new1.length(); i++)
+            System.out.print(new1.at(i)+" ");
+        System.out.println();
+        int []f = {1};
+        DIntArray new2 = new DIntArray(f);
+        new2.add(3);
+        new2.atDelete(1);
+        new2.atDelete(0);
+        new2.atDelete(3);
+        new2.add(2);
+        new2.atDelete(1);
+        new2.add(3);
+        new2.atDelete(1);
+        new2.atDelete(0);
+        new2.atDelete(0);
+        for(int i = 0; i<new2.length(); i++)
+            System.out.print(new2.at(i)+" ");
+        System.out.println();
     }
 }
 
 public class DIntArray {
     private int [] arr;
-    public DIntArray(){}
+    private int k = 0;
+    public DIntArray(){
+        arr = new int[1];
+        k = 1;
+    }
     public DIntArray(int[]a){
         arr = Arrays.copyOf(a,a.length);
+        k = 0;
     }
 
     public int length(){
         return arr.length;
     }
     public void add(int num){
-        int [] arr2 = new int [arr.length+1];
-        for(int i = 0; i<arr.length; i++)
-            arr2[i] = arr[i];
-        arr2[arr2.length-1]=num;
-        arr = arr2;
+        if(arr.length == 1 && k == 1){
+            arr[0] = num;
+            k = 0;
+        }
+        else {
+            int[] arr2 = new int[arr.length + 1];
+            for (int i = 0; i < arr.length; i++)
+                arr2[i] = arr[i];
+            arr2[arr2.length - 1] = num;
+            arr = arr2;
+        }
     }
     public void atInsert(int pos, int num){
         int [] arr3 = new int [arr.length+1];
@@ -51,12 +82,16 @@ public class DIntArray {
         arr = arr3;
     }
     public void atDelete(int pos){
-        int [] arr2 = new int [arr.length-1];
-        for(int i = 0; i<pos; i++)
-            arr2[i] = arr[i];
-        for(int i = pos; i<arr2.length; i++)
-            arr2[i] = arr[i+1];
-        arr = arr2;
+        if(pos > arr.length-1)
+            System.out.println("Номер указанной к удалению позиции больше длинны массива");
+        else if(arr.length!=0) {
+            int[] arr2 = new int[arr.length - 1];
+            for (int i = 0; i < pos; i++)
+                arr2[i] = arr[i];
+            for (int i = pos; i < arr2.length; i++)
+                arr2[i] = arr[i + 1];
+            arr = arr2;
+        }
     }
     public int at(int pos){
         if(pos<arr.length)
