900edee89ad69f58b13d727e900eac0f2a33303d
Solo300m <solo300m@yandex.ru>
2020-03-30 17:11:00 +0400

    H7 Домашнее задание

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -2,12 +2,15 @@ package ru.progwards.java1.lessons.bigints;
 class Program{
     public static void main(String[] args) {
         AbsInteger i1 = new IntInteger(5);
+        AbsInteger i2 = new IntInteger(15);
         AbsInteger sh1 = new ShortInteger((short) 12);
         AbsInteger b1 = new ByteInteger((byte) 10);
         System.out.println(i1);
         System.out.println(sh1);
         System.out.println(b1);
-        System.out.println(AbsInteger.add(i1,sh1).n1);
+        System.out.println(AbsInteger.add(b1,b1).n1);
+        System.out.println(AbsInteger.add(b1,b1).getClass().toString());
+
     }
 }
 public abstract class AbsInteger {
@@ -16,9 +19,55 @@ public abstract class AbsInteger {
         this.n1 = a;
     }
     static AbsInteger add(AbsInteger num1, AbsInteger num2){
-        AbsInteger rez = new IntInteger(0);
-        rez.n1 =  num1.n1 + num2.n1;
+        if(AbsInteger.substrClass(num1,"IntInteger") == true
+                & (AbsInteger.substrClass(num2,"IntInteger") == true
+                |AbsInteger.substrClass(num2,"ShortInteger") == true
+                |AbsInteger.substrClass(num2,"ByteInteger") == true )){
+            AbsInteger rez = new IntInteger(0);
+            rez.n1 =  num1.n1 + num2.n1;
+            return rez;
+        }
+        else if(AbsInteger.substrClass(num2,"IntInteger") == true
+                & (AbsInteger.substrClass(num1,"IntInteger") == true
+                |AbsInteger.substrClass(num1,"ShortInteger") == true
+                |AbsInteger.substrClass(num1,"ByteInteger") == true )){
+            AbsInteger rez = new IntInteger(0);
+            rez.n1 =  num1.n1 + num2.n1;
+            return rez;
+        }
+        else if(AbsInteger.substrClass(num1,"ShortInteger") == true
+                & (AbsInteger.substrClass(num2,"ShortInteger") == true
+                |AbsInteger.substrClass(num2,"ByteInteger") == true )) {
+            AbsInteger rez = new ShortInteger((short) 0);
+            rez.n1 =  num1.n1 + num2.n1;
+            return rez;
+        }
+        else if(AbsInteger.substrClass(num2,"ShortInteger") == true
+                & (AbsInteger.substrClass(num1,"ShortInteger") == true
+                |AbsInteger.substrClass(num1,"ByteInteger") == true )) {
+            AbsInteger rez = new ShortInteger((short) 0);
+            rez.n1 =  num1.n1 + num2.n1;
+            return rez;
+        }
+        else if(AbsInteger.substrClass(num1,"ByteInteger") == true
+                & AbsInteger.substrClass(num2,"ByteInteger") == true ) {
+            AbsInteger rez = new ByteInteger((byte) 0);
+            rez.n1 =  num1.n1 + num2.n1;
+            return rez;
+        }
+        else {
+            AbsInteger rez = new IntInteger(0);
+            return rez;
+        }
+
+    }
+
+    static boolean substrClass(AbsInteger num, String type){
+        String num1str = "";
+        num1str = num.getClass().toString();
+        boolean rez = (num1str.contains(type));
         return rez;
     }
+
     public abstract String toString();
 }
