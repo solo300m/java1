6d50d2ef26160fbc731992e17f4d550ebd28b19f
Solo300m <solo300m@yandex.ru>
2020-04-19 18:15:11 +0400

    H13 Домашнее задание

--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -4,7 +4,29 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
+class Pp{
+    public static void main(String[] args) {
+        List<Shop> shops = new ArrayList<>();
+        List<Product> prod1 = new ArrayList(List.of("art-2","art-6","art-8","art-10"));
+        List<Product> prod2 = new ArrayList(List.of("art-3","art-8","art-9","art-10"));
+        List<Product> prod3 = new ArrayList(List.of("art-2","art-5","art-10"));
+        List<Product> prodControl = new ArrayList(List.of("art-1","art-2","art-3",
+                "art-4","art-5","art-6","art-7","art-8","art-9","art-10"));
+        Shop magasin1 = new Shop(prod1);
+        Shop magasin2 = new Shop(prod2);
+        Shop magasin3 = new Shop(prod3);
+        shops.add(magasin1);
+        shops.add(magasin2);
+        shops.add(magasin3);
+        ProductAnalytics analit = new ProductAnalytics(prodControl,shops);
+        var a = analit.existInAll();
+        var b = analit.notExistInShops();
+        var d = analit.existOnlyInOne();
+        //System.out.println(a);
+        //System.out.println(b);
+        System.out.println(d);
+    }
+}
 public class ProductAnalytics {
         private List<Shop> shops = new ArrayList<>();
         private List<Product> products = new ArrayList<>();
@@ -13,7 +35,7 @@ public class ProductAnalytics {
             this.products.addAll(products);
         }
         public Set<Product> existInAll(){
-            Set<Product> goods = new HashSet<>();
+            Set<Product> goods = new HashSet<>(this.products);
             for(int i = 0; i < shops.size(); i++){
                 goods.retainAll(shops.get(i).getProducts());
             }
@@ -31,19 +53,24 @@ public class ProductAnalytics {
             for(int i = 0; i < shops.size(); i++) {
                 goods.addAll(shops.get(i).getProducts());
             }
-            Set<Product> tmp = new HashSet<>();
-            for(int i = 0; i < shops.size(); i++) {
-                tmp.retainAll(shops.get(i).getProducts());
-            }
-            goods.removeAll(tmp);
-            return goods;
+            Set<Product> tmp = new HashSet<>(this.products);
+            tmp.removeAll(goods);
+            return tmp;
         }
         public Set<Product> existOnlyInOne(){
             Set<Product> goods = new HashSet<>();
-            for(int i = 0; i < shops.size(); i++){
-                goods.removeAll(shops.get(i).getProducts());
+            Set<Product> del = new HashSet<>();
+            for(int i = 0; i < shops.get(i).getProducts().size(); i++) {
+                Product a = shops.get(i).getProducts().get(i);
+                for(int j = 1; j< shops.get(j).getProducts().size(); j++){
+                    Product b = shops.get(j).getProducts().get(j);
+                    if(a.equals(b))
+                        del.add(a);
+               }
             }
-            return goods;
+                //goods.addAll(shops.get(j).getProducts());
+
+            return del;
         }
 }
 
