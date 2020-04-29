e4a508c159b5332deaf6abdf0875b6815c794c2c
Solo300m <solo300m@yandex.ru>
2020-04-29 20:22:52 +0400

    H15 Домашнее задание

--- src/ru/progwards/java1/lessons/maps/FiboMapCache.java
+++ src/ru/progwards/java1/lessons/maps/FiboMapCache.java
@@ -24,15 +24,12 @@ public class FiboMapCache {
         if(cacheOn == true) {
             if (fiboCache.containsKey(n))
                 return fiboCache.get(n);
-            else if (n >= 3 && fiboCache.containsKey(n - 1) && fiboCache.containsKey(n - 2)) {
-                tmp = fiboCache.get(n - 2);
-                tmp2 = fiboCache.get(n - 1);
-                tmp = tmp.add(tmp2);
-                fiboCache.put(n, tmp);
+            else if (n >= 3 && fiboCache.containsKey(n)) {
+                tmp = fiboCache.get(n);
                 return tmp;
             } else if (n < 3) {
                 return tmp;
-            } else if (n >= 3 && (!fiboCache.containsKey(n - 1) || !fiboCache.containsKey(n - 2))) {
+            } else if (n >= 3 && !fiboCache.containsKey(n)) {
                 BigDecimal start = new BigDecimal(1);
                 BigDecimal end = new BigDecimal(1);
                 BigDecimal fibo = new BigDecimal(0);
@@ -48,7 +45,6 @@ public class FiboMapCache {
         }
         else{
             if (n < 3) {
-
                 return tmp;
             }
             else {
@@ -96,5 +92,54 @@ public class FiboMapCache {
     public Map<Integer, BigDecimal> getFiboCache() {
         return fiboCache;
     }
+   /* public BigDecimal fiboNumber(int n){
+        BigDecimal tmp = new BigDecimal(1);
+        BigDecimal tmp2 = new BigDecimal(1);
+        if(fiboCache == null)
+            fiboCache = new HashMap<>();
+        if(cacheOn == true) {
+            if (fiboCache.containsKey(n))
+                return fiboCache.get(n);
+            else if (n >= 3 && fiboCache.containsKey(n - 1) && fiboCache.containsKey(n - 2)) {
+                tmp = fiboCache.get(n - 2);
+                tmp2 = fiboCache.get(n - 1);
+                tmp = tmp.add(tmp2);
+                fiboCache.put(n, tmp);
+                return tmp;
+            } else if (n < 3) {
+                return tmp;
+            } else if (n >= 3 && (!fiboCache.containsKey(n - 1) || !fiboCache.containsKey(n - 2))) {
+                BigDecimal start = new BigDecimal(1);
+                BigDecimal end = new BigDecimal(1);
+                BigDecimal fibo = new BigDecimal(0);
+                for (int i = 3; i <= n; i++) {
+                    fibo = start.add(end);
+                    start = end;
+                    end = fibo;
+                }
+                tmp = fibo;
+                fiboCache.put(n, tmp);
+                return tmp;
+            }
+        }
+        else{
+            if (n < 3) {
 
+                return tmp;
+            }
+            else {
+                BigDecimal start = new BigDecimal(1);
+                BigDecimal end = new BigDecimal(1);
+                BigDecimal fibo = new BigDecimal(0);
+                for (int i = 3; i <= n; i++) {
+                    fibo = start.add(end);
+                    start = end;
+                    end = fibo;
+                }
+                tmp = fibo;
+                return tmp;
+            }
+        }
+        return tmp;
+    }*/
 }
