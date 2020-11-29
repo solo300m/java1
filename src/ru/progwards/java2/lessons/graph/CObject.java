73badbe6eb84ef2b5696de8dd900edd55f772f15
Solo300m <solo300m@yandex.ru>
2020-11-29 15:31:09 +0400

    H12 Продвинутый уровень. Графы. Домашнее задание

---
+++ src/ru/progwards/java2/lessons/graph/CObject.java
@@ -0,0 +1,8 @@
+package ru.progwards.java2.lessons.graph;
+
+import java.util.List;
+
+public class CObject {//Вспомогательный объект
+    public List<CObject> references;
+    int mark;
+}
---
+++ src/ru/progwards/java2/lessons/graph/FindUnused.java
@@ -0,0 +1,12 @@
+package ru.progwards.java2.lessons.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindUnused {
+    public static List<CObject> find(List<CObject> roots, List<CObject> objects){
+        List<CObject>find = new ArrayList<CObject>();
+
+        return find;
+    }
+}
